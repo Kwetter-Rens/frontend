89d5d183293bbd942a4c9cf774a433337da6afa2
rens-kloosterman <rens.kloosterman@student.fontys.nl>
2022-12-11 15:35:32 +0100

    Update app.module.ts

--- kwetter-frontend/src/app/app.module.ts
+++ kwetter-frontend/src/app/app.module.ts
@@ -23,8 +23,32 @@ import { AuthHttpInterceptor } from '@auth0/auth0-angular';
     BrowserModule,
     AppRoutingModule,
     AuthModule.forRoot({
+      // The domain and clientId were configured in the previous chapter
       domain: 'dev-c4buukuwiplm7c1f.us.auth0.com',
-      clientId: '8EhzdG7Z5LGAR64pw4y5YazHH1Rkpzxs'
+      clientId: '8EhzdG7Z5LGAR64pw4y5YazHH1Rkpzxs',
+
+      // Request this audience at user authentication time
+      audience: 'https://dev-c4buukuwiplm7c1f.us.auth0.com/api/v2/',
+
+      // Request this scope at user authentication time
+      scope: 'read:current_user',
+
+      // Specify configuration for the interceptor
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Match any request that starts 'https://dev-c4buukuwiplm7c1f.us.auth0.com/api/v2/' (note the asterisk)
+            uri: 'https://dev-c4buukuwiplm7c1f.us.auth0.com/api/v2/*',
+            tokenOptions: {
+              // The attached token should target this audience
+              audience: 'https://dev-c4buukuwiplm7c1f.us.auth0.com/api/v2/',
+
+              // The attached token should have these scopes
+              scope: 'read:current_user'
+            }
+          }
+        ]
+      }
     }),
     HttpClientModule,
   ],
